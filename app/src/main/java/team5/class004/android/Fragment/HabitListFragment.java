8f60a69b024415e8b7eece5446bd0a2e6d9143e2
SpongeCake <sunrivs@gmail.com>
2019-04-09 13:16:23 +0900

    recyclerview

--- app/src/main/java/team5/class004/android/Fragment/HabitListFragment.java
+++ app/src/main/java/team5/class004/android/Fragment/HabitListFragment.java
@@ -3,19 +3,32 @@ package team5.class004.android.fragment;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import team5.class004.android.R;
 import team5.class004.android.databinding.FragmentHabitListBinding;
+import team5.class004.android.databinding.ItemHabitListBinding;
+import team5.class004.android.model.HabitItem;
 
 public class HabitListFragment extends Fragment {
     View rootView;
     FragmentHabitListBinding fragmentBinding;
     Activity mActivity = getActivity();
+    MyRecyclerAdapter adapter;
+    ArrayList<HabitItem> habitItems = new ArrayList<>();
+
 
     public HabitListFragment()
     {
@@ -34,6 +47,51 @@ public class HabitListFragment extends Fragment {
         fragmentBinding.setFragment(this);
         rootView = fragmentBinding.getRoot();
 
+        fragmentBinding.recyclerView.setLayoutManager(new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false));
+        fragmentBinding.recyclerView.setAdapter(adapter);
+
         return rootView;
+    }
+
+
+
+
+    public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+
+        public MyRecyclerAdapter() {
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_habit_list, viewGroup, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return habitItems.size();
+        }
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            public ItemHabitListBinding itemBinding;
+
+            public ViewHolder(View itemView){
+                super(itemView);
+
+                itemBinding = DataBindingUtil.bind(itemView);
+            }
+
+            @Override
+            public void onClick(View view) {
+                int position = getAdapterPosition();
+            }
+        }
     }
 }
---
+++ app/src/main/java/team5/class004/android/model/HabitItem.java
@@ -0,0 +1,13 @@
+package team5.class004.android.model;
+
+public class HabitItem {
+    public String _id;
+    public String brand;
+    public String title;
+    public String content;
+    public String image;
+    public String type;
+    public int shippingPrice;
+    public int level;
+//    public ShoppingProductItem shoppingProduct;
+}
