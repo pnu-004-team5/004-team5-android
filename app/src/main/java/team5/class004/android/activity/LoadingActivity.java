cc9d2f8abbe53388a8f9f38f9c514883c07b7f7b
SHIM-YEWON <shimyewon42@gmail.com>
2019-04-30 22:52:23 +0900

    Loading 화면 추가

---
+++ app/src/main/java/team5/class004/android/activity/LoadingActivity.java
@@ -0,0 +1,20 @@
+package team5.class004.android.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
